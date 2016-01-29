d68c21033d2b17a15e3e2f782b9e4d5a7d6415ff
Brian Dean Jennings <brian@bdjnews.com>
2016-01-29 23:42:14 +1300

    Zooom

---
+++ Assets/Scripts/PictureGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using JetBrains.Annotations;
+using UnityEditor;
+
+public class PictureGenerator : ScriptableSingleton<PictureGenerator> {
+	[UsedImplicitly] public GameObject PictureTemplate;
+	[UsedImplicitly] public int Count = 3;
+	[UsedImplicitly] public float ScaleDelay = 3;
+
+	private readonly List<GameObject> m_PictureList = new List<GameObject>();
+
+	[UsedImplicitly]
+	private void Update () {
+		if (m_PictureList.Count < Count && (m_PictureList.Count == 0 || m_PictureList.Last().transform.localScale.x > ScaleDelay)) {
+			m_PictureList.Add(Instantiate(PictureTemplate));
+		}
+	}
+
+	[UsedImplicitly]
+	public static void Remove(GameObject obj) {
+		if (!instance.m_PictureList.Contains(obj)) {
+			return;
+		}
+
+		instance.m_PictureList.Remove(obj);
+		Destroy(obj);
+	}
+}
---
+++ Assets/Scripts/PictureZoomer.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class PictureZoomer : MonoBehaviour {
+	[UsedImplicitly] public float Rate = 0.1f;
+	[UsedImplicitly] public float Division = 10f;
+
+	[UsedImplicitly]
+	private void Update() {
+		float scale = transform.localScale.x;
+		scale = scale + ((scale/Division + Rate)*Time.deltaTime);
+		transform.localScale = new Vector3(scale, scale);
+	}
+}
