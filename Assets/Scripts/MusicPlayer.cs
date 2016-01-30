5ed372d2462161eb6b457a4cb295a6ff9f1e1f7b
Brian Dean Jennings <brian@bdjnews.com>
2016-01-31 01:22:37 +1300

    Added music by Podington Bear

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+	private int m_SongIndex;
+
+	private AudioSource AudioSource {
+		get { return m_AudioSource ? m_AudioSource : (m_AudioSource = this.GetOrAddComponent<AudioSource>()); }
+	}
+	private AudioSource m_AudioSource;
+
+	private List<AudioClip> Songs {
+		get { return m_Songs ?? (m_Songs = Resources.LoadAll<AudioClip>("Audio/Songs").OrderBy(c => Guid.NewGuid()).ToList()); }
+	} 
+	private List<AudioClip> m_Songs;
+
+	[UsedImplicitly]
+	void Update () {
+		if (AudioSource.isPlaying) {
+			return;
+		}
+
+		m_SongIndex++;
+
+		if (m_SongIndex >= Songs.Count) {
+			m_SongIndex = 0;
+		}
+
+		AudioSource.PlayOneShot(Songs[m_SongIndex]);
+	}
+}
