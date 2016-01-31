5d1ddb32453b8ce1a248147ce6e9706900b7cca1
Brian Dean Jennings <brian@bdjnews.com>
2016-02-01 00:03:24 +1300

    Added custom cursor / Removed some songs to reduce build size

---
+++ Assets/Scripts/CustomCursor.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour {
+	[UsedImplicitly] public Texture2D CursorTexture;
+	[UsedImplicitly] public CursorMode CursorMode = CursorMode.Auto;
+	[UsedImplicitly] public Vector2 CursorSpot = Vector2.zero;
+
+	[UsedImplicitly]
+	private void Start() {
+		Cursor.SetCursor(CursorTexture, CursorSpot, CursorMode);
+	}
+}
